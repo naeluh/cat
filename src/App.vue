6a5692783e4dca41d0338153c4c6b8d15f38c25e
Nick Hulea <naeluh@gmail.com>
2018-05-25 21:44:07 -0700

    tara demo working needs refactor before work

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,13 @@
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+html, body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: #FFF000;
+}
+</style>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- PhysicsJS dev branch -->
-  <canvas ref="viewport" id="viewport"></canvas>
+  <canvas ref="viewport" id="viewport" height="804"></canvas>
 </template>
 
 <script>
@@ -18,23 +18,107 @@ export default {
       return this.$refs.viewport
     }
   },
-  created () {
-    console.log(Physics)
-    console.log(Physics.world)
-    Physics(function (world) {
-      console.log(world)
-    })
-  },
+  created () {},
   mounted () {
-    console.log(Physics(function (world) {}))
-    console.log(Physics.world)
+    // console.log(Physics(function (world) {}))
+    // console.log(Physics.world)
     console.log(this.el)
-    Physics(function (world) {
-      console.log(world)
+    // let scratch = Physics.scratchpad()
+    Physics(world => {
+      console.log(window.innerHeight)
+      // bounds of the window
+      let viewportBounds = Physics.aabb(0, -200, window.innerWidth, window.innerHeight)
+      let edgeBounce
+      let renderer
+      // create a renderer
+      renderer = Physics.renderer('canvas', {
+        el: this.$refs.viewport,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        autoResize: false,
+        meta: false // don't display meta data
+      })
+      // add the renderer
+      world.add(renderer)
+      // render on each step
+      world.on('step', () => {
+        world.render()
+      })
+      // constrain objects to these bounds
+      edgeBounce = Physics.behavior('edge-collision-detection', {
+        aabb: viewportBounds,
+        restitution: 0.8,
+        cof: 0.8
+      })
+      // resize events
+      window.addEventListener('resize', () => {
+        console.log(window.innerHeight)
+        // as of 0.7.0 the renderer will auto resize... so we just take the values from the renderer
+        viewportBounds = Physics.aabb(0, 0, renderer.width, window.innerHeight)
+        // update the boundaries
+        edgeBounce.setAABB(viewportBounds)
+      }, true)
+      // create some bodies
+      world.add(Physics.body('circle', {
+        x: renderer.width * 0.4,
+        y: renderer.height * 0.3,
+        vx: 0.3,
+        vy: 0.9,
+        radius: 80,
+        styles: {
+          src: require('@/assets/cat_3.png'),
+          width: 272,
+          height: 274
+        }
+      }))
+      world.add(Physics.body('circle', {
+        x: renderer.width * 0.7,
+        y: renderer.height * 0.3,
+        vx: 0.4,
+        vy: 0.8,
+        radius: 80,
+        styles: {
+          src: require('@/assets/cat_1.png'),
+          width: 198,
+          height: 343
+        }
+      }))
+      // add some fun interaction
+      const attractor = Physics.behavior('attractor', {
+        order: 3,
+        strength: 0.008
+      })
+      world.on({
+        'interact:poke' (pos) {
+          world.wakeUpAll()
+          attractor.position(pos)
+          world.add(attractor)
+        },
+        'interact:move' (pos) {
+          attractor.position(pos)
+        },
+        'interact:release' () {
+          world.wakeUpAll()
+          world.remove(attractor)
+        }
+      })
+      // add things to the world
+      world.add([
+        Physics.behavior('interactive', {
+          el: renderer.container
+        }), Physics.behavior('constant-acceleration'), Physics.behavior('sweep-prune'), Physics.behavior('body-impulse-response'), edgeBounce
+      ])
+      // subscribe to ticker to advance the simulation
+      Physics.util.ticker.on(time => {
+        world.step(time)
+      })
     })
+    // scratch.done()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#viewport { position: relative; height: 100%;}
+</style>
