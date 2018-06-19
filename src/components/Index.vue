fa88ffc7c4dfaf0d4b8671b4ccd538c6b38f6b26
Nick Hulea <naeluh@gmail.com>
2018-06-18 21:50:55 -0700

    trying matter seems to be better

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import P5 from 'p5/lib/p5.min'
+import Matter from 'matter-js'
 
 export default {
   name: 'Index',
@@ -43,186 +43,114 @@ export default {
   created () {},
   mounted () {
     let self = this
-    const myp5 = new P5(function (sketch) {
-      let i = null
-      let j = null
-      let color = null
-      let mouseIsPressed = false
-      let isSlowing = false
-      sketch.mousePressed = function () {
-        mouseIsPressed = true
-        isSlowing = true
-      }
-      sketch.mouseReleased = function () {
-        mouseIsPressed = false
-      }
-      sketch.windowResized = function () {
-        sketch.resizeCanvas(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio)
-      }
-      sketch.preload = function () {
-        for (let i = 0; i < self.cats.length; i++) {
-          let r = 5 + Math.round(sketch.random() * 35)
-          let x = r + Math.round(sketch.random() * (sketch.displayWidth - 2 * r))
-          let y = 0
-          let c = sketch.color(40, 60, 160, 200)
-          self.catz.push(new Cat(x, y, r, c, i))
+    // window.onload = function () {
+    console.log(Matter)
+    const Cats = null || {}
+    Cats.sprites = function () {
+      let Engine = Matter.Engine
+      let Render = Matter.Render
+      let Runner = Matter.Runner
+      let Composites = Matter.Composites
+      let Common = Matter.Common
+      let MouseConstraint = Matter.MouseConstraint
+      let Mouse = Matter.Mouse
+      let World = Matter.World
+      let Bodies = Matter.Bodies
+      // create engine
+      let engine = Engine.create()
+      let world = engine.world
+      // create renderer
+      let render = Render.create({
+        element: self.$refs.sketch,
+        engine: engine,
+        options: {
+          width: window.innerWidth,
+          height: window.innerHeight,
+          background: 'transparent',
+          showAngleIndicator: false,
+          wireframes: false
         }
+      })
+      Render.run(render)
+      // create runner
+      let runner = Runner.create()
+      Runner.run(runner, engine)
+      // add bodies
+      let options = {
+        isStatic: true
       }
-      sketch.setup = function () {
-        console.log(window.innerWidth)
-        this.canvas = sketch.createCanvas(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio)
-        this.canvas.parent(self.$refs.sketch)
-        sketch.noStroke()
-      }
-      sketch.draw = function () {
-        sketch.clear()
-        sketch.background('rgba(255,255,255, 0.0)')
-        // Draw Cats
-        for (i = 0; i < self.catz.length; i++) {
-          self.catz[i].display()
-        }
-        for (i = 0; i < self.catz.length; i++) {
-          if (self.catz[i] !== self.selectedBall) {
-            self.catz[i].dy += self.gravity
-          }
-          for (j = i; j < self.catz.length; j++) {
-            self.catz[i].collide(self.catz[j])
-            self.catz[i].rotateCat(self.catz[j])
-          }
-        }
-        if (mouseIsPressed) {
-          if (!self.selectedBall) {
-            for (i = 0; i < self.catz.length; i++) {
-              if (self.catz[i].selected()) {
-                self.selectedBall = self.catz[i]
-                break
+      world.bodies = []
+      // these static walls will not be rendered in this sprites Cats, see options
+      World.add(world, [
+        Bodies.rectangle(document.documentElement.clientWidth / 2, 0, document.documentElement.clientWidth, 100, options),
+        Bodies.rectangle(document.documentElement.clientWidth / 2, document.documentElement.clientHeight, document.documentElement.clientWidth, 100, options),
+        Bodies.rectangle(document.documentElement.clientWidth, document.documentElement.clientHeight / 2, 100, document.documentElement.clientHeight, options),
+        Bodies.rectangle(0, document.documentElement.clientHeight / 2, 100, document.documentElement.clientHeight, options)
+      ])
+      let stack = Composites.stack(20, 20, 2, 2, 0, 0, function (x, y) {
+        if (Common.random() > 0.35) {
+          return Bodies.rectangle(x, y, 64, 64, {
+            render: {
+              strokeStyle: '#ffffff',
+              sprite: {
+                texture: self.cats[1]
               }
             }
-          } else {
-            self.selectedBall.dx = (sketch.mouseX - self.selectedBall.x)
-            self.selectedBall.dy = (sketch.mouseY - self.selectedBall.y)
-          }
+          })
         } else {
-          self.selectedBall = false
-        }
-
-        // Move balls
-        for (i = 0; i < self.catz.length; i++) {
-          let cat = self.catz[i]
-          cat.move()
-          cat.bounce()
-        }
-        color = sketch.get(sketch.mouseX, sketch.mouseY)
-        // sketch.clear()
-      }
-      function Cat (x, y, r, c, index) {
-        this.x = x
-        this.y = y
-        this.r = r
-        this.c = c
-        this.dx = 10 * (sketch.random() - 0.5)
-        this.dy = 10 * (sketch.random() - 0.5)
-        this.rotation = 0.01
-        this.rotationSpeed = 0.5
-        this.isSlowing = false
-        this.minSpeed = 0
-        this.maxSpeed = 0.05
-        this.rx = 0.05
-        this.ry = 0.05
-        self.cats[index] = sketch.loadImage(self.cats[index])
-        this.rotateCat = function (that) {
-          if (isSlowing) {
-            this.rx *= self.drag
-            this.ry *= self.drag
-            this.rx -= self.acceleration * 10
-            this.ry -= self.acceleration * 10
-            if (this.rotation < this.minSpeed && this.rotation > 0) {
-              this.rotation = this.minSpeed
-              isSlowing = false
+          return Bodies.circle(x, y, 46, {
+            density: 1,
+            frictionAir: 0.06,
+            restitution: 0.3,
+            friction: 0.01,
+            render: {
+              sprite: {
+                texture: self.cats[0]
+              }
             }
-          } else {
-            this.rx *= self.drag
-            this.ry *= self.drag
-            this.rx += 0.05
-            this.ry += 0.05
-            if (this.rotation < -this.maxSpeed && this.rotation > this.maxSpeed && this.rotation < 0) {
-              this.rotation = this.maxSpeed
-              isSlowing = true
-            } else {
-              this.rotation = sketch.atan2(this.ry, this.rx)
-            }
+          })
+        }
+      })
+      World.add(world, stack)
+      // add mouse control
+      let mouse = Mouse.create(render.canvas)
+      let mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false
           }
         }
-        this.move = function () {
-          this.dx *= self.drag
-          this.dy *= self.drag
-          this.x += this.dx
-          this.y += this.dy
+      })
+      World.add(world, mouseConstraint)
+      // keep the mouse in sync with rendering
+      render.mouse = mouse
+      // fit the render viewport to the scene
+      Render.lookAt(render, {
+        min: {
+          x: 0,
+          y: 0
+        },
+        max: {
+          x: window.innerWidth,
+          y: window.innerHeight
         }
-        this.bounce = function () {
-          isSlowing = true
-          if (this.x < this.r) {
-            this.x = this.r
-            this.dx *= -self.friction / 10
-            this.rx *= -self.friction
-            isSlowing = false
-          }
-          if (this.x > sketch.displayWidth - this.r) {
-            this.x = sketch.displayWidth - this.r
-            this.dx *= -self.friction / 10
-            this.rx *= -self.friction
-            isSlowing = false
-          }
-          if (this.y < this.r - sketch.displayHeight * 10) {
-            this.y = this.r - sketch.displayHeight * 10
-            this.dy *= -self.friction / 10
-            this.ry *= -self.friction
-            isSlowing = false
-          }
-          if (this.y > sketch.displayHeight - self.cats[index].height + 90) {
-            this.y = sketch.displayHeight - self.cats[index].height + 90
-            this.dy *= -self.friction
-            this.ry *= -self.friction
-            isSlowing = true
-          }
-        }
-        this.selected = function () {
-          if (sketch.abs(sketch.mouseX - this.x) < self.cats[index].width / 4 && sketch.abs(sketch.mouseY - this.y) < self.cats[index].height / 2) {
-            if (color[0] !== 0 && color[1] !== 0 && color[2] !== 0) {
-              isSlowing = false
-              return true
-            }
-          }
-        }
-        this.collide = function (that) {
-          let dx = this.x - that.x
-          let dy = this.y - that.y
-          let dr = this.r + that.r
-
-          if (dx * dx + dy * dy < dr * dr) {
-            isSlowing = false
-            let theta = sketch.atan2(dy, dx)
-            let force = (dr - sketch.sqrt(dx * dx + dy * dy))
-            this.dx += force * sketch.cos(theta)
-            that.dx -= force * sketch.cos(theta)
-            this.dy += force * sketch.sin(theta)
-            that.dy -= force * sketch.sin(theta)
-          }
-        }
-        this.display = function () {
-          sketch.push()
-          sketch.angleMode(sketch.RADIANS)
-          sketch.rectMode(sketch.CENTER)
-          sketch.imageMode(sketch.CENTER)
-          sketch.translate(this.x, this.y)
-          sketch.rotate(this.rx)
-          sketch.image(self.cats[index], 0, 0, 0, 0)
-          sketch.pop()
+      })
+      // context for MatterTools.Demo
+      return {
+        engine: engine,
+        runner: runner,
+        render: render,
+        canvas: render.canvas,
+        stop: function () {
+          Matter.Render.stop(render)
+          Matter.Runner.stop(runner)
         }
       }
-    })
-    console.log(myp5)
-    console.log(window)
+    }
+    Cats.sprites()
+    // }
   }
 }
 </script>
